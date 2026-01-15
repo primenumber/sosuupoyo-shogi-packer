0eb70cd9390020d1af0c7da4111d684e49275893
primenumber <prime@kmc.gr.jp>
2026-01-16 08:09:09 +0900

    Split files

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod packer;
 mod pdep;
 mod pext;
 mod position;
+mod sspfv1;
 
-pub use packer::{Packer, SSPFv1};
+pub use packer::Packer;
 pub use position::{Bitboard, Color, Hand, Piece, PieceKind, Position, SfenParseError, Square};
+pub use sspfv1::SSPFv1;
